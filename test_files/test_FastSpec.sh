5068ddb5e1d2fe702aa830392ec15609a855e483
magdasolitro <magda.solitro@icloud.com>
2022-09-08 16:07:53 +0200

    Added toy example for spectre-v1 gadget

--- test_files/test_FastSpec.sh
+++ test_files/test_FastSpec.sh
@@ -5,4 +5,5 @@
 set -euo pipefail
 
 # Compile needed libraries
-gcc ./openssl/crypto/aes/aes_ofb.c ./openssl/crypto/aes/aes_core.c ./openssl/crypto/modes/ofb128.c testing_AES_OFB_nosymbolic.c -o testing_AES_OFB_nosymbolic.o -I./openssl/crypto/aes 
+# gcc ./openssl/crypto/aes/aes_ofb.c ./openssl/crypto/aes/aes_core.c ./openssl/crypto/modes/ofb128.c testing_AES_OFB_nosymbolic.c -o testing_AES_OFB_nosymbolic.o -I./openssl/crypto/aes 
+gcc -std=c11 -pedantic toy.c -o toy.o
