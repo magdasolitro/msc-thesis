b957139afe913092dfa9fe4fe171db630a5b635b
magdasolitro <magda.solitro@icloud.com>
2022-09-08 15:50:35 +0200

    Splitted test file between two instances for both software projects, added compilation step for FastSpec-specific files

---
+++ test_files/test_FastSpec.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Setting bash as "if it throws an error, abort right now and don't go on with the script"
+# to make sure you're not looking at analysis from previous file binaries version
+set -euo pipefail
+
+# Compile needed libraries
+gcc ./openssl/crypto/aes/aes_ofb.c ./openssl/crypto/aes/aes_core.c ./openssl/crypto/modes/ofb128.c testing_AES_OFB_nosymbolic.c -o testing_AES_OFB_nosymbolic.o -I./openssl/crypto/aes 
---
+++ test_files/testing_AES_OFB_nosymbolic.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <openssl/aes.h>
+#include <openssl/modes.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <openssl/crypto.h>
+#include "aes_locl.h"
+
+int main(){
+    const unsigned char in[AES_BLOCK_SIZE] = "Hello, world!";
+    unsigned char out[sizeof(in)];    
+    size_t len = sizeof(in);
+    const unsigned char key[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55,
+        0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
+    AES_KEY aes_key;
+    unsigned char ivec[AES_BLOCK_SIZE];
+
+    // holds the state information in a form of the number of bytes used out of the total block size.
+    int *num=0;
+
+    AES_set_encrypt_key(key, 128, &aes_key);
+
+    AES_ofb128_encrypt((const unsigned char *) in, out, len, 
+        (const AES_KEY *) &aes_key, ivec, num);
+}
